82044c123f265963f7e96b51eb7902f7f6a3280c
Ankita <annetra@iu.edu>
2018-09-24 08:45:18 -0400

    New repository

---
+++ work.php
@@ -0,0 +1,23 @@
+<?php
+
+echo '<pre>';
+
+echo "bar {$n}\n";
+
+class Animal{
+  public $type = 'Pig';
+}
+
+$wilbur = new Animal();
+echo $wilbur->type;
+
+$arr = [
+  'first' => 'Tom',
+  'last' => 'Gregory'
+];
+
+
+
+
+
+ ?>
