5577a093a488764b182a4ea64a7c4e34153ac4ad
Ankita <annetra@iu.edu>
2018-10-03 16:08:51 -0400

    Debugging

--- app/common.php
+++ app/common.php
@@ -3,12 +3,9 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
-var_dump($_POST);
-
 if($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'],'application/json') !== false) {
-  $_POST = json_decode(file_get_contents('php://input',true));
-  var_dump($_POST);
+  $_POST = json_decode(file_get_contents('php://input'),true);
 }
 
 require 'environment.php';
